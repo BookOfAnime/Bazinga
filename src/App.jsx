55e6ff64257dbf58139d6c493f0166798c7826af
Ephriam Kassa <ephriam123@gmail.com>
2024-06-02 14:47:26 -0700

    dss

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
       <nav style={{ backgroundColor: '#282c34', padding: '1rem' }}>
         <div cstyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <a href="https://t.me/BazingaBlackPortal" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>TELEGRAM</a>
-          <a href="https://www.pump.fun" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>PUMP.FUN</a>
+          <a href="https://pump.fun/HBr4GBXUFaVMGWKsTbzUQRJpS2aFLfTgLZi9V1JLpump" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>PUMP.FUN</a>
           <a href="https://x.com/BazingaBlackSol" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>X</a>
         </div>
       </nav>
-      
+
     </div>
   )
 }
