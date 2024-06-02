59a2ab134886dabcab5d31938b794c47e49ad6f5
Ephriam Kassa <ephriam123@gmail.com>
2024-06-02 13:44:14 -0700

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='con'>
       <nav style={{ backgroundColor: '#282c34', padding: '1rem' }}>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <a href="https://www.pump.fun" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>TELEGRAM</a>
@@ -15,7 +15,7 @@ function App() {
           <a href="https://x.com/BazingaBlackSol" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>X</a>
         </div>
       </nav>
-    </>
+    </div>
   )
 }
 
