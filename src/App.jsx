808bab811f33cd0683236ec33ce53bd1bc6ba492
Ephriam Kassa <ephriam123@gmail.com>
2024-06-02 13:54:51 -0700

    dsdsds

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className='con'>
       <nav style={{ backgroundColor: '#282c34', padding: '1rem' }}>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <a href="https://www.pump.fun" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>TELEGRAM</a>
+          <a href="https://x.com/BazingaBlackSol" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>TELEGRAM</a>
           <a href="https://www.pump.fun" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>PUMP.FUN</a>
           <a href="https://x.com/BazingaBlackSol" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>X</a>
         </div>
